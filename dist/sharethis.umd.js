cbf2d921af559589e1360d1b2214a0ef393c6edf
rudolphk2907 <rudolph.koegelenberg@gmail.com>
2020-04-25 14:31:14 +0200

    build

---
+++ dist/sharethis.umd.js
@@ -0,0 +1,199 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.CloudflareVideoPlayer = {}));
+}(this, (function (exports) { 'use strict';
+
+  //
+  //
+  //
+  //
+
+  var script = {
+    name: "Sharethis",
+    props: {
+      shareThisElementId: {
+        type: String,
+        default: "st-2"
+      },
+      shareThisEmbedUrl: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        default: window.location.href
+      }
+    },
+    beforeDestroy: function beforeDestroy() {
+      var element = document.getElementById(this.shareThisElementId);
+      if (element) {
+        element.style.display = "none";
+      }
+    },
+    mounted: function mounted() {
+      var this$1 = this;
+
+      if (window.__sharethis__) {
+        window.__sharethis__.href = this.url;
+        var element = document.getElementById(this.shareThisElementId);
+        if (element) {
+          element.style.display = "block";
+        }
+      } else {
+        var shareScript = document.createElement("script");
+        shareScript.setAttribute("src", this.shareThisEmbedUrl);
+        shareScript.setAttribute("async", "true");
+        shareScript.onload = function () {
+          window.__sharethis__.href = this$1.url;
+        };
+        document.head.appendChild(shareScript);
+      }
+    }
+  };
+
+  function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+  /* server only */
+  , shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+      createInjectorSSR = createInjector;
+      createInjector = shadowMode;
+      shadowMode = false;
+    } // Vue.extend constructor export interop.
+
+
+    var options = typeof script === 'function' ? script.options : script; // render functions
+
+    if (template && template.render) {
+      options.render = template.render;
+      options.staticRenderFns = template.staticRenderFns;
+      options._compiled = true; // functional template
+
+      if (isFunctionalTemplate) {
+        options.functional = true;
+      }
+    } // scopedId
+
+
+    if (scopeId) {
+      options._scopeId = scopeId;
+    }
+
+    var hook;
+
+    if (moduleIdentifier) {
+      // server build
+      hook = function hook(context) {
+        // 2.3 injection
+        context = context || // cached call
+        this.$vnode && this.$vnode.ssrContext || // stateful
+        this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+        // 2.2 with runInNewContext: true
+
+        if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+          context = __VUE_SSR_CONTEXT__;
+        } // inject component styles
+
+
+        if (style) {
+          style.call(this, createInjectorSSR(context));
+        } // register component module identifier for async chunk inference
+
+
+        if (context && context._registeredComponents) {
+          context._registeredComponents.add(moduleIdentifier);
+        }
+      }; // used by ssr in case component is cached and beforeCreate
+      // never gets called
+
+
+      options._ssrRegister = hook;
+    } else if (style) {
+      hook = shadowMode ? function () {
+        style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+      } : function (context) {
+        style.call(this, createInjector(context));
+      };
+    }
+
+    if (hook) {
+      if (options.functional) {
+        // register for functional component in vue file
+        var originalRender = options.render;
+
+        options.render = function renderWithStyleInjection(h, context) {
+          hook.call(context);
+          return originalRender(h, context);
+        };
+      } else {
+        // inject component registration as beforeCreate hook
+        var existing = options.beforeCreate;
+        options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+      }
+    }
+
+    return script;
+  }
+
+  var normalizeComponent_1 = normalizeComponent;
+
+  /* script */
+  var __vue_script__ = script;
+
+  /* template */
+  var __vue_render__ = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div')};
+  var __vue_staticRenderFns__ = [];
+
+    /* style */
+    var __vue_inject_styles__ = undefined;
+    /* scoped */
+    var __vue_scope_id__ = undefined;
+    /* module identifier */
+    var __vue_module_identifier__ = undefined;
+    /* functional template */
+    var __vue_is_functional_template__ = false;
+    /* style inject */
+    
+    /* style inject SSR */
+    
+
+    
+    var component = normalizeComponent_1(
+      { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+      __vue_inject_styles__,
+      __vue_script__,
+      __vue_scope_id__,
+      __vue_is_functional_template__,
+      __vue_module_identifier__,
+      undefined,
+      undefined
+    );
+
+  function install(Vue) {
+    if (install.installed) { return }
+    install.installed = true;
+    Vue.component('Sharethis', component);
+  }
+
+  var plugin = {
+    install: install,
+  };
+
+  var GlobalVue = null;
+  if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+  } else if (typeof global !== 'undefined') {
+    GlobalVue = global.vue;
+  }
+
+  if (GlobalVue) {
+    GlobalVue.use(plugin);
+  }
+
+  component.install = install;
+
+  exports.default = component;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
